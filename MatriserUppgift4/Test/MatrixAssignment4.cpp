e9e2b46ce726956c7c691dd2fef017a966ab7cc3
godofnanners <casper.martensson@telia.com>
2020-01-22 12:47:19 +0100

    done with matrix

---
+++ MatriserUppgift4/Test/MatrixAssignment4.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MatrixAssignment4
+{
+	TEST_CLASS(MatrixAssignment4)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
